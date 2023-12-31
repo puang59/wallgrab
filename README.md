fb4af4eee1281570d8afa88731d63bd924076e9d
puang59 <kk61468@gmail.com>
2024-01-01 00:27:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# wallgrab
+
+This tool is designed to fetch and download wallpapers from [Wallhaven](https://wallhaven.cc) based on various user-defined parameters. It uses the Wallhaven API to search for wallpapers and downloads them to the user's local system.
+
+## Features
+
+- **Interactive User Input**: The script prompts the user for various parameters like search query, categories, purity, sorting options, resolutions, and more.
+- **Customizable Downloads**: Users can specify the number of wallpapers they want to download.
+- **Flexible Parameters**: Supports a wide range of search parameters available on Wallhaven.
+
+## Requirements
+
+- `jq` (A lightweight and flexible command-line JSON processor)
+- `wget` (For downloading the wallpapers)
+- `curl` (For sending HTTP requests)
+
+## How to Use
+
+1. **Clone the Repository**:
+   ```bash
+   git clone https://github.com/puang59/wallgrab.git
+   ```
+2. **Navigate to the Directory**:
+
+   ```bash
+   cd wallgrab
+   ```
+
+3. **Make the Script Executable**:
+
+   ```bash
+   chmod +x wallgrab.sh
+   ```
+
+4. **Run the Script**:
+
+   ```bash
+   ./wallgrab.sh
+   ```
+
+5. **Follow the On-Screen Prompts**: Answer the prompts to specify your search criteria.
+
+## Parameters
+
+- **Query**: Enter keywords/tags for the wallpapers.
+- **Categories**: Specify the categories (e.g., general, anime, people).
+- **Purity**: Choose the purity level (e.g., sfw, sketchy, nsfw).
+- **Sorting**: Select the sorting method (e.g., date added, relevance, random).
+- **Order**: Choose the order (ascending or descending).
+- **Top Range**: Specify the time range for top wallpapers (e.g., 1d, 3d, 1w).
+- **At Least Resolution**: Specify the minimum resolution (e.g., 1920x1080).
+- **Resolutions**: Specify resolutions (comma-separated).
+- **Ratios**: Specify aspect ratios (e.g., 16x9, 16x10).
+- **Colors**: Specify colors (comma-separated hex codes).
+- **Page**: Specify the page number for pagination.
+- **Seed**: (Optional) Specify a seed for random results.
+- **Threshold**: Specify the number of wallpapers to download.
+
+Note: Detailed description is available [here](https://wallhaven.cc/help/api)
+
+## Output
+
+The downloaded wallpapers will be saved in the `~/wallpapers` directory by default.
+To change this behaviour, just modify [line 21](https://github.com)
+
+```bash
+    mkdir -p ~/wallpapers
+```
---
+++ wallgrab.sh
@@ -0,0 +1,172 @@
+# Function to prompt user for input and handle skipping
+prompt_input() {
+    read -p "$1 (Press Enter to skip): " input
+    if [[ -z "$input" ]]; then
+        echo "skip"
+    else
+        echo "$input"
+    fi
+}
+
+# Function to download wallpapers
+download_wallpapers() {
+    local response="$1"
+    local threshold="$2"
+    local counter=0
+
+    echo "$response" | jq -c '.data[]' | while read -r row; do
+        IMAGE_URL=$(echo "$row" | jq -r '.path')
+        if [[ ! -z "$IMAGE_URL" ]]; then
+            ((counter++))
+            mkdir -p ~/wallpapers
+            wget -q --show-progress -P ~/wallpapers "$IMAGE_URL"
+            if [[ $counter -eq $threshold ]]; then
+                break
+            fi
+        fi
+    done
+}
+
+# Prompt user for inputs
+QUERY=$(prompt_input "Enter query (tagname / -tagname / +tag1 +tag2 / etc.)")
+CATEGORIES=$(prompt_input "Enter categories (100/101/111/etc. - 1 for on, 0 for off)")
+PURITY=$(prompt_input "Enter purity (100/110/111/etc. - 1 for on, 0 for off)")
+SORTING=$(prompt_input "Enter sorting (date_added/relevance/random/views/favorites/toplist)")
+ORDER=$(prompt_input "Enter order (desc/asc)")
+TOPRANGE=$(prompt_input "Enter topRange (1d/3d/1w/1M/3M/6M/1y)")
+ATLEAST=$(prompt_input "Enter atleast resolution (e.g., 1920x1080)")
+RESOLUTIONS=$(prompt_input "Enter resolutions (comma-separated e.g., 1920x1080,1920x1200)")
+RATIOS=$(prompt_input "Enter ratios (e.g., 16x9,16x10)")
+COLORS=$(prompt_input "Enter colors (e.g., 660000,990000,cc0000,...)")
+PAGE=$(prompt_input "Enter page (e.g., 1)")
+SEED=$(prompt_input "Enter seed (optional)")
+
+# Prompt user for threshold
+THRESHOLD=$(prompt_input "Enter the number of wallpapers you want to download")
+if [[ "$THRESHOLD" == "skip" ]]; then
+    THRESHOLD=0
+fi
+
+# Base API endpoint
+BASE_URL="https://wallhaven.cc/api/v1/search"
+URL="$BASE_URL"
+
+# Constructing the API endpoint based on parameters
+if [[ "$QUERY" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL?q=$QUERY"
+    else
+        URL="$URL&q=$QUERY"
+    fi
+fi
+
+if [[ "$CATEGORIES" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL?categories=$CATEGORIES"
+    else
+        URL="$URL&categories=$CATEGORIES"
+    fi
+fi
+
+if [[ "$PURITY" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL?purity=$PURITY"
+    else
+        URL="$URL&purity=$PURITY"
+    fi
+fi
+
+if [[ "$SORTING" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL?sorting=$SORTING"
+    else
+        URL="$URL&sorting=$SORTING"
+    fi
+fi
+
+if [[ "$ORDER" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&order=$ORDER"
+    else
+        URL="$URL&order=$ORDER"
+    fi
+fi
+
+if [[ "$TOPRANGE" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&topRange=$TOPRANGE"
+    else
+        URL="$URL&topRange=$TOPRANGE"
+    fi
+fi
+
+if [[ "$ATLEAST" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&atleast=$ATLEAST"
+    else
+        URL="$URL&atleast=$ATLEAST"
+    fi
+fi
+
+if [[ "$RESOLUTIONS" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&resolutions=$RESOLUTIONS"
+    else
+        URL="$URL&resolutions=$RESOLUTIONS"
+    fi
+fi
+
+if [[ "$RATIOS" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&ratios=$RATIOS"
+    else
+        URL="$URL&ratios=$RATIOS"
+    fi
+fi
+
+if [[ "$COLORS" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&colors=$COLORS"
+    else
+        URL="$URL&colors=$COLORS"
+    fi
+fi
+
+if [[ "$PAGE" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&page=$PAGE"
+    else
+        URL="$URL&page=$PAGE"
+    fi
+fi
+
+if [[ "$SEED" != "skip" ]]; then
+    if [[ "$URL" == *'search' ]]; then
+        URL="$URL&seed=$SEED"
+    else
+        URL="$URL&seed=$SEED"
+    fi
+fi
+
+# Debug: Print the constructed URL
+echo "Constructed URL: $URL"
+
+# Send request and save the response
+RESPONSE=$(curl -s "$URL")
+
+# Check if the response is valid
+if [[ $? -ne 0 ]]; then
+    echo "Failed to fetch data from the API."
+    exit 1
+fi
+
+# Check if the 'data' field exists in the JSON response
+if ! echo "$RESPONSE" | jq -e '.data' > /dev/null; then
+    echo "No 'data' field found in the API response."
+    exit 1
+fi
+
+# Download wallpapers
+download_wallpapers "$RESPONSE" "$PAGE"
+
+echo "Images downloaded successfully to ~/wallpapers"
